0daa610352d3cf6ade48758e5091cf753cb73fb3
ench3r <ench3r@gmail.com>
2023-03-22 17:17:01 +0200

    Initial commit

---
+++ src/main/java/tuvarna/si/TicketCenterApplication.java
@@ -0,0 +1,13 @@
+package tuvarna.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tuvarna/si/TicketCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package tuvarna.si;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
